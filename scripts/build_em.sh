88f4747c7c112827b489a9b6afdd59148aaaa558
will-nix <electromanx11@gmail.com>
2026-01-23 17:51:57 -0800

    Build system configurationx

---
+++ scripts/build_em.sh
@@ -0,0 +1,1 @@
+emcmake cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B ../embuild
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+cp ../embuild/graphing.js server/graphing.js
+cp ../embuild/graphing.wasm server/graphing.wasm
+live-server -H localhost ./server -p 41245
